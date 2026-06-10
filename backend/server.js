f16cc289f78b6c1ed4295fa5040865342c6f5cc6
rifqyaris <rifqy.535240136@stu.untar.ac.id>
2026-06-11 02:20:50 +0700

    progress

--- backend/server.js
+++ backend/server.js
@@ -23,21 +23,12 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // Kalau nanti error lagi, ganti "*" dengan link Vercel lu
+    origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://project-uas-soft-dev.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-
-app.options("*", cors());
+app.use(cors()); 
 app.use(express.json({ limit: "10mb" })); 
 
 const USERS_DB = [
